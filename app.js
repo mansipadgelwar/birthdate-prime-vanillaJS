1b2e8196dc722c9cdc0e26f32e47b46c4d4e96e3
mansipadgelwar <mpadgelwar@gmail.com>
2021-09-16 08:47:52 +0530

    feat: add regExp to validate date

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ var txtOutput = document.querySelector("#txt-output");
 
 btnValidate.addEventListener("click",getDate);
 
+var regEx = /^(0[1-9]|1\d|2\d|3[01])\/(0[1-9]|1[0-2])$/;
+
 var numberOfDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 function getDate() {
     var userName = txtName.value;
     var userBirthDate = txtInput.value;  
-    var birthDate = userBirthDate.split('/');
-    var DD = birthDate[0];
-    var MM = birthDate[1];
-
+  
     if(userName === "" && userBirthDate === ""){
       txtOutput.innerText = "Please enter all the details."
     }
@@ -23,18 +22,24 @@ function getDate() {
     else if(userBirthDate === ""){
       txtOutput.innerText = "Please enter your birthdate."
     }  
-    else if (DD < 31 || MM < 12 || DD > 0 || MM > 0 || (!isNaN(DD)) || (!isNaN(MM))) {
-      if (DD < numberOfDays[MM - 1]) {
-        checkPrime(DD);
+    else if(validateDate(userBirthDate)){
+      var birthDate = userBirthDate.split('/');
+      var DD = Number(birthDate[0]);
+      var MM = Number(birthDate[1]);
+      if (DD <= numberOfDays[MM - 1]){
+          checkPrime(DD);
       }
-      else {
-        txtOutput.innerText = "Error !! Invalid date format."
+      else{
+        txtOutput.innerText = " ERROR !! INVALID DATE FORMAT.";
+         
       }
-  
+      
     }
-    else {
-      txtOutput.innerText = "Error !! Invalid date format."
+    else{
+      txtOutput.innerText = " ERROR !! INVALID DATE FORMAT.";
+         
     }
+    
   }
 
 
@@ -49,7 +54,7 @@ function getDate() {
       }
   
       if (isPrime) {
-        txtOutput.innerText = "Hey, your birth date is a prime number";
+        txtOutput.innerText = "Hey, your birth date is a prime number. You can share it on your social media.";
       }
       else {
         txtOutput.innerText = "Hey, your birth date is not a prime number";
@@ -59,3 +64,10 @@ function getDate() {
      txtOutput.innerText = "Hey, your birth date is not a prime number";
     }
   }
+
+  function validateDate(date){
+    var flag = regEx.test(date);
+    return flag;
+  }
+
+
